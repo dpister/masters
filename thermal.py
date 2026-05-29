20ea44caefd9a2ed1c1fa4042c2836bb97715592
Daniel Pister <pister.daniel@gmail.com>
2026-05-29 13:40:08 +0200

    Add new file for thermal calculations

---
+++ thermal.py
@@ -0,0 +1,22 @@
+import math
+from math_helper import number
+import numpy as np
+
+
+def calculate_canonical_partition_sum(temperature: number, eigenvalues: np.ndarray) -> number:
+    z = 0
+    for eigenvalue in eigenvalues:
+        z += math.exp(-1 / temperature * eigenvalue)
+    return z
+
+
+def calculate_thermal_expectation_value(
+    temperature: number,
+    canonical_sum: number,
+    expectation_values: np.ndarray,
+    eigenvalues: np.ndarray,
+) -> number:
+    result = 0
+    for expectation_value, eigenvalue in zip(expectation_values, eigenvalues):
+        result += math.exp(-eigenvalue / temperature) * expectation_value
+    return result / canonical_sum
